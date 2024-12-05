e60e86a4f7f90538c872d17561fa6db45c2da5c4
Anna <anyalar04@gmail.com>
2024-12-05 21:56:30 +0300

    refactor: move initializeDatabase to the end

--- src/app.ts
+++ src/app.ts
@@ -15,12 +15,6 @@ const app = express();
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 3000;
 
-initializeDatabase().then(() => {
-  app.listen(PORT, () =>
-    console.log(`Server is running on http://${HOST}:${PORT}`)
-  );
-});
-
 app.use(cors());
 app.use(express.json());
 app.use('/api/boards', boardRoutes);
@@ -28,3 +22,9 @@ app.use('/api/lists', listRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/logs', userActionLogsRoutes);
 app.use(errorHandler);
+
+initializeDatabase().then(() => {
+  app.listen(PORT, () =>
+    console.log(`Server is running on http://${HOST}:${PORT}`)
+  );
+});
